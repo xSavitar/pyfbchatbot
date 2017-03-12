6f5dcb05e62fbd69c7dfb0a37badf47b86292c57
Alangi Derick <alangiderick@gmail.com>
2017-03-12 11:47:46 +0100

    Tokens and Comments on Bot
    * Adding the necessary tokens that will be used by the
      callback webhooks to receive incoming messages.
    
    * Commenting out the code to help users understand what
      it does.
    
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>

--- server.py
+++ server.py
@@ -1,11 +1,17 @@
+# Import Flask and request module from Flask
 from flask import Flask, request
+
+# Import request
 import requests
 
+# create a flask app instance
 app = Flask(__name__)
 
-ACCESS_TOKEN = ""
-VERIFY_TOKEN = ""
+# Tokens from the facebook page web hooks
+ACCESS_TOKEN = "EAAUzNdZCxv8oBAMNkk15UhxLkDjP5nCsbizuBDUDWl2pkAxwhAUZAktOXwdyWZAZBy1jsZAPb2DFRI7iI10Ozm5BzHXYaW94JG2CRoZBPoU3dHOtb0XF2KRmiWQpDmq0AGcFazVPFAytST3uVQKwmDfCYfe7m8dKN87a35kbdwegZDZD"
+VERIFY_TOKEN = "secret"
 
+# method to reply to a message from the sender
 def reply(user_id, msg):
     data = {
         "recipient": {"id": user_id},
@@ -14,7 +20,7 @@ def reply(user_id, msg):
     resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
     print(resp.content)
 
-
+# GET request to handle the verification of tokens
 @app.route('/', methods=['GET'])
 def handle_verification():
     if request.args['hub.verify_token'] == VERIFY_TOKEN:
@@ -22,7 +28,7 @@ def handle_verification():
     else:
         return "Invalid verification token"
 
-
+# POST request to handle in coming messages then call reply()
 @app.route('/', methods=['POST'])
 def handle_incoming_messages():
     data = request.json
